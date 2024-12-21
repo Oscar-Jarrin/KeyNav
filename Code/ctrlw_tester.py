a761ec1bc495c3e54b7677813bfd2c425237745d
Oscar Blanco <ohbjart@gmail.com>
2024-12-21 15:29:31 -0300

    updating functionalities documentation

--- Code/ctrlw_tester.py
+++ Code/ctrlw_tester.py
@@ -4,15 +4,12 @@ keys_quee = set()
 
 def instructionShortcutsListener(key):
     keys_quee.add(key)
-    print(f"{key} was pressed")
-
-    if Key.f in keys_quee and (key == 'w' or key.char == 'w'):
-        print("Ctrl + W was pressed!")
 
 def instructionShorcutExit(key):
     keys_quee.discard(key)
     if key == Key.esc:
         return False
-
-with Listener(on_press=instructionShortcutsListener, on_release=instructionShorcutExit) as listener: 
+    if key == Key.media_volume_up:
+        print("volume up")
+with Listener(on_press=instructionShortcutsListener, on_release=instructionShorcutExit, suppress=True) as listener: 
     listener.join()
