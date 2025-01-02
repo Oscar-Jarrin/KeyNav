6b76e6e0e9e37680e42ea8016c282daf6f374e0f
Oscar Blanco <ohbjart@gmail.com>
2025-01-02 20:46:54 -0300

    updating class diagram, requierements and code

--- Code/Scroller.py
+++ Code/Scroller.py
@@ -7,27 +7,24 @@ class Scroller:
         self.listener = None
         self.context = context 
         self.mouse = Controller() 
-
+        print("the scroller has been created")
     def start(self):
         keys_queue = self.keys_queue
         context = self.context
         mouse = self.mouse
         def press(key):
             keys_queue.add(key)
-
-        def release(key):
             if(Key.alt_l in keys_queue and key == KeyCode.from_char('c')):
                 print("alt + c was pressed")
-                context.setDetectorState() #CREATE THE CONTEXT CLASS AND ADD THE SETMANAGERSTATE() METHOD
-            if (Key.ctrl_l in keys_queue):
-                if key == Key.media_volume_up:
-                    print("scroll up")
-                    mouse.scroll(0,3) 
-                elif key == Key.media_volume_down:
-                    print("scroll down")
-                    mouse.scroll(0, -3) 
-            if key == Key.esc:
-                return False
+                context.setToDetectorState() #CREATE THE CONTEXT CLASS AND ADD THE SETMANAGERSTATE() METHOD
+        print("the scroller has started")
+        def release(key):
+            if key == Key.media_volume_up or key == KeyCode.from_char('k'):
+                print("scroll up")
+                mouse.scroll(0,1) 
+            elif key == Key.media_volume_down or key == KeyCode.from_char('j'):
+                print("scroll down")
+                mouse.scroll(0, -1) 
             keys_queue.discard(key)
 
         self.listener = Listener(on_press=press, on_release=release, suppress=True)
